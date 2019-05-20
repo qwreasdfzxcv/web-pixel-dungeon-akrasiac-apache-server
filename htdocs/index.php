70ec9c834eb52cbe4cdae4b8fa2a6604b58c6640
tjlee <dldlxowns@naver.com>
2019-05-20 19:49:35 +0900

    make index.php

---
+++ htdocs/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Web Pixel Akrasiac</title>
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="" />
+  </head>
+
+  <body class="index">
+    <h3>Scoring</h3>
+          <ul>
+            <li><a href="dashboard/top_scores.html">top scores</a></li>
+            <li><a href="dashboard/winners.html">winners</a></li>
+          </ul>
+  </body>
+</html>
