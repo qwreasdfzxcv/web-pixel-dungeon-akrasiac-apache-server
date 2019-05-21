f819ecbdc68b39ebc8ad5f5c1abef86796db160e
tjlee <dldlxowns@naver.com>
2019-05-22 08:35:37 +0900

    fix PHP_connection.php fix miss char

--- htdocs/PHP_connection.php
+++ htdocs/PHP_connection.php
@@ -6,13 +6,13 @@ if(mysqli_connect_errno($con)){
   echo "Failed to connect to MySQL: ", mysqli_connect_error();
 }
 
-mysql_set_charset($con, "utf8");
+mysqli_set_charset($con, "utf8");
 
 $res = mysqli_query($con, "select * from rankinfo");
 $result = array();
 
-while($row = mysqli_fatch_array($res)){
-  array_push($result, array('gmaeID'=>$row[0], 'rank'=>$row[1], 'score'=>$row[2], 'player'=>$row[3], 'class'=>$row[4], 'subclass'=>$row[5], 'depth'=>$row[6], 'end'=>$row[7], 'level'=>$row[8], 'turns'=>$row[9], 'version'=>$row[10]));
+while($row = mysqli_fetch_array($res)){
+  array_push($result, array('gmaeID'=>$row[0], 'rank'=>$row[1], 'score'=>$row[2], 'player'=>$row[3], 'class'=>$row[4], 'subclass'=>$row[5], 'depth'=>$row[6], 'end'=>$row[7]));
 }
 
 echo json_encode(array("result"=>$result));
