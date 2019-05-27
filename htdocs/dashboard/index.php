ff530c54f0db2e59f78b898b9b12e7df7d145e96
tjlee <dldlxowns@naver.com>
2019-05-27 17:58:17 +0900

    make index.php

---
+++ htdocs/dashboard/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'user.php';
+
+$username="";
+$password="";
+
+if(isset($_POST['username'])){
+  $username=$_POST['username'];
+}
+
+if(isset($_POST['password'])){
+  $password=$_POST['password']
+}
+ ?>
